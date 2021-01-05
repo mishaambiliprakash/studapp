5d98e15396607ba0e2c49b4614872eb399a790c6
mishaambiliprakash <ab224105@gmail.com>
2021-01-05 11:26:06 +0530

    clickevent

--- app/src/main/java/com/misha/studentapp/MainActivity.kt
+++ app/src/main/java/com/misha/studentapp/MainActivity.kt
@@ -2,10 +2,16 @@ package com.misha.studentapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun mybtnclick(view: View) {
+        Toast.makeText(this,"test",Toast.LENGTH_LONG).show();
     }
 }
